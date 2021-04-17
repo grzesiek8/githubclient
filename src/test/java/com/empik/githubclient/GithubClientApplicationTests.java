5c59881aa65aae519da35872f89376d03d232c36
Grzegorz Macurek <grzegorzmacurek@carfax.com>
2021-04-17 17:54:42 +0200

    Initial commit

---
+++ src/test/java/com/empik/githubclient/GithubClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empik.githubclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
